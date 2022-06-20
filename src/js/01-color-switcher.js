d050c1ed14f1c0ff3a972d62e3d96cbb5e28cb40
Alexey Yermachkov <o.yermachkov@gmail.com>
2022-06-21 02:45:12 +0300

    adds finished  1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,3 @@
-
 // Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
 
 // Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
@@ -10,19 +9,24 @@ const refs = {
   stop: document.querySelector('[data-stop]'),
 };
 
+let intervalId = null;
+
 refs.start.addEventListener('click', onStartClick);
 refs.stop.addEventListener('click', onStopClick);
 
 
 function onStartClick() {
-setInterval(function() {
+  refs.start.setAttribute('disabled', 'disabled');
+intervalId = setInterval(function() {
   refs.body.style.backgroundColor = getRandomHexColor();
 }, 1000);
 }
 
-// function onStopClick() {
+function onStopClick() {
+  clearInterval(intervalId);
+  refs.start.removeAttribute('disabled', 'disabled');
   
-// }
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
